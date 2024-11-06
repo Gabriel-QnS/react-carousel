5828afb19960f755ce0296245424d015167517fa
Gabriel R. <gabrielrengifo211@gmail.com>
2024-11-06 17:14:16 -0500

    Controls implemented

--- src/carousel.js
+++ src/carousel.js
@@ -1,31 +1,70 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function Carousel(){
-    let [currentIndex, setCurrentIndex] = useState(0); // initializes state, function to change index of pic
-    const pics = [];
-    for (let i = 1; i <= 5; i++){
-        pics.push('./assets/'+`img${i}.jpg`)
-    };// populates array of pics with the directions
+function CarouselControls({ currentIndex, setCurrentIndex, pics }) {
+  const [isPlaying, setIsPlaying] = useState(true);
 
-    useEffect(() => {
-        const change = setInterval(() =>{
-            if(currentIndex === 5){
-                setCurrentIndex(0)
-            } else { setCurrentIndex(currentIndex++) }
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % pics.length);
+  };
 
-        }, 3000);
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      (prevIndex - 1 + pics.length) % pics.length
+    );
+  };
 
-        return () => clearInterval(change)
-    });
+  const handleStop = () => {
+    setIsPlaying(false);
+  };
 
-    console.log(pics)
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
 
-    return(
-        <div className='carousel m-auto'>
-            <img src={pics[currentIndex]} alt="A string of numbers"></img>
-        </div>
-    )
-    
+  useEffect(() => {
+    let interval;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        handleNext();
+      }, 3000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isPlaying, currentIndex, pics.length]);
+
+  return (
+    <div className="carousel-controls p-3">
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
+      {isPlaying ? (
+        <button onClick={handleStop}>Stop</button>
+      ) : (
+        <button onClick={handlePlay}>Play</button>
+      )}
+    </div>
+  );
+}
+
+function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const pics = [];
+
+  for (let i = 1; i <= 5; i++) {
+    pics.push(`./assets/img${i}.jpg`);
+  }
+
+  return (
+    <div className="carousel-container p-4">
+      <div className="carousel m-auto border border-light border-5">
+        <img src={pics[currentIndex]} alt="A string of numbers" />
+        <CarouselControls
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        pics={pics}
+      />
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
