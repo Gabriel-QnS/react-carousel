ef69999d31c08f7a864d4806e5fc85e24aeb6618
Gabriel R. <gabrielrengifo211@gmail.com>
2024-11-05 23:15:57 -0500

    in work condition

--- src/carousel.js
+++ src/carousel.js
@@ -4,12 +4,15 @@ function Carousel(){
     let [currentIndex, setCurrentIndex] = useState(0); // initializes state, function to change index of pic
     const pics = [];
     for (let i = 1; i <= 5; i++){
-        pics.push('/assets/' + `img${i}.jpg`)
+        pics.push('./assets/' + `img${i}.jpg`)
     };// populates array of pics with the directions
 
     useEffect(() => {
         const change = setInterval(() =>{
-            setCurrentIndex(currentIndex++)
+            if(currentIndex == 5){
+                setCurrentIndex(0)
+            } else { setCurrentIndex(currentIndex++) }
+
         }, 3000);
 
         return () => clearInterval(change)
